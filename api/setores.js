3adc0ad9c4e963025ab6b862f1fcbbbbb6af2c9e
johnnyvaz1 <johnnyjohnnyjohnnyvaz@gmail.com>
2019-08-31 23:14:37 -0300

    setores ok

--- api/setores.js
+++ api/setores.js
@@ -2,26 +2,26 @@ module.exports = app => {
     const { existsOrError } = app.api.validation
 
     const save = (req, res) => {
-        const setors = {...req.body }
+        const setores = {...req.body }
 
-        if (req.params.id) setors.id = req.params.id
+        if (req.params.id) setores.id = req.params.id
 
         try {
-            existsOrError(setors.setor, 'Nome do setor não informado')
+            existsOrError(setores.setor, 'Nome do setor não informado')
         } catch (msg) {
             res.status(400).send(msg)
         }
 
-        if (setors.id) {
-            app.db('setors')
-                .update(setors)
-                .where({ id: setors.id })
+        if (setores.id) {
+            app.db('setores')
+                .update(setores)
+                .where({ id: setores.id })
                 .then(_ => res.status(204).send())
                 .catch(err => res.status(500).send(err))
         } else {
-            app.db('setors')
-                .insert(setors)
-                .then(_ => res.status(201).send({ "status":true, setors}))
+            app.db('setores')
+                .insert(setores)
+                .then(_ => res.status(201).send({ "status":true, setores}))
                 .catch(err => res.status(500).send(err))
         }
     }
@@ -30,13 +30,13 @@ module.exports = app => {
         const page = req.query.page || 1
         const limit = req.query.limit || 10
 
-        const result = await app.db('setors').count('id').first()
+        const result = await app.db('setores').count('id').first()
         const count = result
 
-        app.db('setors')
+        app.db('setores')
             //.select('id', 'nome', 'endereco', 'numero', 'bairro', 'email', 'facebook', 'dataNasc')
             .limit(limit).offset(page * limit - limit)
-            .then(setors => res.json({ data:  setors, count, limit, page }))
+            .then(setores => res.json({ data:  setores, count, limit, page }))
             .catch(err => res.status(500).send(err))
     }
 
